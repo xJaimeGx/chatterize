87d52425ff48ffc1a7aad6496c253f48aa0c81cb
kierstenv <kierstenvicknair22@gmail.com>
2022-05-23 18:56:59 -0500

    add (temp¿) logo

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="flex-row justify-space-between-lg justify-center align-center w-100">
+      <div className="flex-row justify-space-between-lg justify-center align-center w-100 p-10">
           <Link to="/">
             <h1>Chatterize!</h1>
           </Link>
