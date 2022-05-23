ada8fc5a2f9bfb840a06e082f9f2dd160c8b38eb
kierstenv <kierstenvicknair22@gmail.com>
2022-05-23 02:54:21 -0500

    style header and card outlines

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-          <h1>Chatterize</h1>
-        </Link>
+          <Link to="/">
+            <h1>Chatterize!</h1>
+          </Link>
 
-        <nav className="text-center">
+        <nav>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
         </nav>
