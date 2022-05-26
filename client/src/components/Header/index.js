71ed9e34dd0ac26c10abfab28a35e173f26c548f
Jack Chamberlain <jack.chamberlain11@yahoo.com>
2022-05-25 19:50:24 -0500

    navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,16 +4,21 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="flex-row justify-space-between-lg justify-center align-center w-100 p-10">
-          <Link to="/">
-            <h1>Chatterize!</h1>
-          </Link>
-
-        <nav>
-          <Link to="/login">Log In</Link>
-          <Link to="/signup">Sign Up</Link>
-        </nav>
+    <nav className= "navbar">
+      <a href="#" class="logo">Chatterize</a>
+      <input type="checkbox" id="toggler"></input>
+      <label for="toggler"><i class="r1-menu-line"></i></label>
+      <div class="menu">
+        <ul class="list">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
       </div>
+    </nav>
+      <a href="#" className="button">
+  <span>Chatterize</span>
+</a>
     </header>
   );
 };
