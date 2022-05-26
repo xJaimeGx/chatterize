d110a57a53453f2c62883bcd346ec7f22e0b3501
Forever Young <dy9040@gmail.com>
2022-05-25 23:35:13 -0500

    added change handle and form submit

---
+++ client/src/components/TopicForm/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const TopicForm = () => {
+const [topicText, setText] = useState('');
+const [characterCount, setCharacterCount] = useState(0);
+
+const handleChange = event => {
+    if (event.target.value.length <= 280) {
+        setText(event.target.value);
+        setCharacterCount(event.target.value.length);
+    }
+};
+
+const handleFormSubmit = async event => {
+    event.preventDefault();
+    setText('');
+    setCharacterCount(0);
+};
+
+    return (
+        <div>
+            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+                Character Count: 0/280
+            </p>
+            <form className="flex-row justify-center justify-space-between-md align-stretch">
+                onSubmit={handleFormSubmit}
+                <textarea
+                placeholder="Here's a new topic..."
+                value={topicText}
+                className="form-input col-12 col-md-9"
+                onChange={handleChange}
+                ></textarea>
+                <button className="btn col-12 col-md-3" type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default TopicForm;
