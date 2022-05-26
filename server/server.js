9718fd61ce8296285e2c9bf536d0ce7c6917a555
kierstenv <kierstenvicknair22@gmail.com>
2022-05-26 09:20:48 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -22,10 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')));
+  app.use(express.static(path.resolve(__dirname, 'client/public')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/public/index.html'));
   });
 }
 
