a5cdadecc099e92db46fa14f0c2e673c4876333b
kierstenv <kierstenvicknair22@gmail.com>
2022-05-26 08:09:14 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -21,11 +21,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.resolve(__dirname, "./client/build")));
     
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+//   });
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, './client/build')));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
   });
 }
 
