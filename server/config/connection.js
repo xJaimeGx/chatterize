d96052fe17dd5fc43e797f696bbeab1e2179df81
Forever Young <dy9040@gmail.com>
2022-06-01 08:11:05 -0500

    added heroku connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chatterize', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost27017/chatterize', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
