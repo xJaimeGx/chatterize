0a4ada7cc69501971e9ec8f5ca555dddd58a2f3f
Forever Young <dy9040@gmail.com>
2022-05-31 23:56:32 -0500

    updated connection.js and package.json to accept heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chatterize', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/chatterize', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
