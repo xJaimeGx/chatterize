62ce42a405c32e2443bf524621a18db8329b1a0a
Forever Young <dy9040@gmail.com>
2022-06-01 08:17:19 -0500

    deploying to heroku.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost27017/chatterize', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/chatterize', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
